90704b00bce023bb7c7fac1a1e7fcf37c52dbc1c
Matthew <matt.lu@mail.utoronto.ca>
2020-08-25 14:40:08 -0400

    Updates

---
+++ precache-manifest.457e7192583d436174f746253305f8b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e722eec36c10558626f8352c2a3b2033",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "0fc2767b4349352dbcb7",
+    "url": "/pokedex/static/css/main.70eec16b.chunk.css"
+  },
+  {
+    "revision": "fb3c14515287f640f517",
+    "url": "/pokedex/static/js/2.4141ac18.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pokedex/static/js/2.4141ac18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fc2767b4349352dbcb7",
+    "url": "/pokedex/static/js/main.cdcc48a2.chunk.js"
+  },
+  {
+    "revision": "12dbb9a7e3426fb8f09a",
+    "url": "/pokedex/static/js/runtime-main.0327efd7.js"
+  }
+]);
